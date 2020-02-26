244a1b25d4bdaab9774b31d3205af3f243662179
cweltonsmith <c.welton.smith@gmail.com>
2020-02-26 14:32:48 -0600

    Login api call working

--- loginSite/Content/scripts/login.js
+++ loginSite/Content/scripts/login.js
@@ -1,9 +1,22 @@
-let loginfunc = async function apifunc(){
 
-	const response = await fetch('https://goodwillomaha-nw2020.azurewebsites.net/')
-	const myJson = await response.json();
-    
-	
-}
+$(document).ready(function () {
+	$("#getLoginInformation").click(function () {
+		var person = new Object();
+		person.loyaltyID = $('#loyaltyID').val();
+		person.password = $('#txtPassword').val();
+		$.ajax({
+			url: 'https://goodwillomaha-nw2020.azurewebsites.net/user/login',
+			type: 'POST',
+			dataType: 'json',
+			data: person,
+			success: function (data, textStatus, xhr) {
+				console.log(data);
+			},
+			error: function (xhr, textStatus, errorThrown) {
+				console.log(errorThrown);
+				console.log(textStatus);
+			}
+		});
+	});
+});
 
-document.querySelector('#getLoginInformation').addEventListener('click', loginfunc);
